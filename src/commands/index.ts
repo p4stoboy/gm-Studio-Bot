db6fd6172b93bcb2c3daba1d16cd4600cdde7ec7
p4stoboy <b1gh@protonmail.com>
2022-12-26 15:02:45 +1100

    tidy up

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,9 +1,12 @@
 import {ApplicationCommandData} from "discord.js";
 import command_objects from "./create_commands";
 
-export const commands_array: ApplicationCommandData[] = [];
-export const commands = new Map<string, ApplicationCommandData>();
+const commands_array: ApplicationCommandData[] = [];
+const commands = new Map<string, ApplicationCommandData>();
 for (let command of command_objects) {
   commands_array.push(command);
   commands.set(command.name, command);
 }
+
+
+export {commands_array, commands};
