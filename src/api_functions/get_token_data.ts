6a38e841e364502fc15056c724fb49692c8429d5
p4stoboy <b1gh@protonmail.com>
2022-12-26 12:21:35 +1100

    downgraded fetch ffs

--- src/api_functions/get_token_data.ts
+++ src/api_functions/get_token_data.ts
@@ -1,7 +1,6 @@
 import {TokenData} from "../types/TokenData";
 import fetch from "node-fetch";
 
-
 export const get_token_data = async (collection_slug: string, token_id: number): Promise<TokenData> => {
   const response = await fetch(`https://api.gmstudio.art/collections/${collection_slug}/token/${token_id}`);
   if (!response.ok) {
