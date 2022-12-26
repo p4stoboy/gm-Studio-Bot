74190f5493af0d7018d81754add6fc29cc0b5761
p4stoboy <b1gh@protonmail.com>
2022-12-27 09:25:23 +1100

    using get param to alternate retry URL for broken requests.

--- src/commands/create_commands.ts
+++ src/commands/create_commands.ts
@@ -1,6 +1,6 @@
 import {collections} from "../collections";
 import {
-  ChatInputCommandInteraction,
+  ChatInputCommandInteraction, EmbedBuilder,
   HexColorString,
   MessageApplicationCommandData,
 } from "discord.js";
@@ -8,6 +8,22 @@ import {choose} from "../helpers";
 import {get_token_data} from "../api_functions/get_token_data";
 import {token_embed} from "../discord_functions/token_embed";
 
+// this checks whether the image loaded in to the embed after 10 seconds, if not it resends the embed alternating between
+// max quality (95) and pretty good (80) until the image resolves.
+// quality gets alternated because the URL needs to change for Discord to retry the request, but we don't want to drop it
+// too low so we alternate.
+const check_retry = async (interaction: ChatInputCommandInteraction, _embed: EmbedBuilder, url: string, side = 1) => {
+  const message = await interaction.fetchReply();
+  if (!message) throw new Error('no message found');
+  const embed = message.embeds[0];
+  console.log(embed);
+  console.log(embed.image);
+  if (embed.image?.width === 0) {
+    await interaction.editReply({embeds: [_embed.setImage((side < 0 ? url : url + '?img-quality=80'))]});
+    console.log('edited');
+    setTimeout(async () => await check_retry(interaction, _embed, url, -side), 10000);
+  }
+}
 
 // function to parse all collections commands and return token to interaction
 const gm_func = async (interaction: ChatInputCommandInteraction) => {
@@ -31,7 +47,9 @@ const gm_func = async (interaction: ChatInputCommandInteraction) => {
   const hex = '#00000000';
   const embed = token_embed(token_data, hex as HexColorString, collection);
 
-  const message = await interaction.editReply({embeds: [embed]});
+  const message = await interaction.editReply({embeds: [embed]}).catch((e) => console.log(e));
+  if (!message) return;
+  setTimeout(async () => await check_retry(interaction, embed, token_data.image), 10000);
 }
 
 const command_objects: MessageApplicationCommandData[] = [];
